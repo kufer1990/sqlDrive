ee87eff05b58d84bb76330414adab865e929e4a8
kufer1990 <94316752+kufer1990@users.noreply.github.com>
2022-01-07 15:25:11 +0100

    second site add

---
+++ js/choiceInstitution.js
@@ -0,0 +1,6 @@
+
+
+document.querySelector('.tdTableItem').addEventListener('click',(e)=>{
+console.log(e);
+console.log('test');
+})
--- php/choiceInstitution.php
+++ php/choiceInstitution.php
@@ -7,18 +7,21 @@ include 'components/connect.php';
 // $conn->exec($SELECT_NEIGH)
 $dbh = $conn->query('SELECT * FROM `okręgi`');
 
-while($row = $dbh->fetch()){
-    global $Neigh;
-    $Neigh = $row[1];
-    echo $Neigh ;
-}
-
-
+// while($row = $dbh->fetch()){
+//     global $Neigh;
+//     $Neigh = $row[1];
+//     echo $Neigh ;
+// }
+// header("Refresh:0.5 ");
 ?>
+<div class="bg-mySite">
+    <!-- <img src="../img/watercolor-ga2e23b0ff_1280.png" alt=""> -->
+    <img src="../img/background-g88c946023_1280.jpg" alt="">
+</div>
 <div class="container">
     <div class="row sectionTitleChoiceInstitution">
         <div class="col-12 text-center mt-5 mb-5">
-            <p>Wybierz Swój Okręg</p>
+            <h1><p>Wybierz Swój Okręg</p></h1>
         </div>
     </div>
     <div class="row justify-content-center">
@@ -26,17 +29,38 @@ while($row = $dbh->fetch()){
             <table class="table text-center">
                 <thead class="thead-dark">
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">OKRĘGI</th>
-                        <th scope="col"></th>
+                        <th scope="col" class='tableItem'>#</th>
+                        <th scope="col" class='tableItem'>OKRĘGI</th>
+                        <th scope="col" class='tableItem'>Wejście</th>
                     </tr>
                 </thead>
                 <tbody>
-                 <tr>
+            <?php 
+            $i=0;
+                while($row=$dbh->fetch()){    
+                    $i++;
+                    global $table; 
+                    $table = $row[1];
+                    echo "<tr>
+                    <th scope='row' class='tableItem'>$i</th>
+                    <td class='tableItem'>";
+                    echo $table;
+                    echo "</td>
+                    <td><i class='far fa-edit tableItem tdTableItem' id='choiceNeigh$i'></i></td>
+                    </tr>";
+                };
+                
+                
+           ?>
+
+
+
+
+                    <!-- <tr>
                         <th scope="row">1</th>
                         <td>Mark</td>
                         <td><i class="far fa-edit"></i></td>
-                    </tr>
+                    </tr> -->
                 </tbody>
             </table>
 
@@ -46,6 +70,6 @@ while($row = $dbh->fetch()){
 
 </body>
 <!-- fild for scripts js -->
-<script src="../js/class.js"></script>
+<script src="../js/choiceInstitution.js"></script>
 <?php
 include 'components/footer.php';
